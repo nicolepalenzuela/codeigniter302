50b90f44526abe02f978f3d6acc991fb44179f61
nicolepalenzuela <ngpalenzuela@student.apc.edu.ph>
2021-06-16 00:53:04 +0800

    updated vanillaget with heroku app url

--- application/views/news/vanillaget.php
+++ application/views/news/vanillaget.php
@@ -5,7 +5,7 @@
     <body>
         <script>
                 const request = new XMLHttpRequest();
-                const url = 'http://localhost/codeigniter/index.php/newsapi';
+                const url = 'https://codeigniter301.herokuapp.com/index.php/newsapi/';
                 request.open("GET", url);
                 request.send();
                 request.onload = (e) => {
