7abddf8d5e127a6d7f2255a690a0f07aa4f3f2ce
nicolepalenzuela <ngpalenzuela@student.apc.edu.ph>
2021-06-16 00:17:38 +0800

    added insert REST api call

---
+++ application/controllers/Newsapi.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class Newsapi extends RestController {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+		$this->load->database();
+    }
+
+	public function index_get($id = 0)
+	{
+        if(!empty($id)){
+            $data = $this->db->get_where('news', ['id' => $id])->row_array();
+        }else{
+            $data = $this->db->get('news')->result();
+        }
+        $this->response($data, 200);
+
+	}
+
+    public function index_post()
+        {
+            $input = $this->input->post();
+            $this->db->insert('news',$input);
+            $this->response(['News created successfully.'],200);
+        } 
+    } 
